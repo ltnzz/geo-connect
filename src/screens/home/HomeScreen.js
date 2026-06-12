3bcc2ee0b97ac72bcc36528a4bae7702d00cc22f
Zephyr-017 <mzr.zidane@contoh.com>
2026-06-12 19:10:31 +0700

    feat: add dummy PostCard component and feed layout to HomeScreen

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -1,11 +1,28 @@
-import TabPlaceholder from '../../components/common/TabPlaceholder';
+import { ScrollView, StyleSheet, View } from 'react-native';
+
+import ScreenHeader from '../../components/common/ScreenHeader';
+import PostCard from '../../components/post/PostCard';
+import { colors, spacing } from '../../utils/theme';
 
 export default function HomeScreen() {
   return (
-    <TabPlaceholder
-      icon="home-outline"
-      subtitle="Posts from people and places around you will appear here."
-      title="Home"
-    />
+    <View style={styles.screen}>
+      <ScreenHeader />
+      <ScrollView contentContainerStyle={styles.listContent} showsVerticalScrollIndicator={false}>
+        <PostCard />
+        <PostCard />
+        <PostCard />
+      </ScrollView>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    backgroundColor: colors.background,
+    flex: 1,
+  },
+  listContent: {
+    padding: spacing.md,
+  },
+});

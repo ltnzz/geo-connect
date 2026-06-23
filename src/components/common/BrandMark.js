cf6d4931b447ad1f77ad41b4ca3fcc0109d151df
Latanza <latanzaakbarfadilah@gmail.com>
2026-06-23 17:41:16 +0700

    style: fix BrandMark aspect ratio to reduce empty vertical padding

--- src/components/common/BrandMark.js
+++ src/components/common/BrandMark.js
@@ -4,12 +4,13 @@ import { Image, StyleSheet, Text, View } from 'react-native';
 import { radius } from '../../utils/theme';
 
 export default function BrandMark({ size = 88 }) {
+  const height = size * 0.76; // 1744/2292 aspect ratio
   return (
     <Image
       accessibilityLabel="AroundU logo"
       source={require('../../../assets/logo-fix.png')}
       style={{
-        height: size,
+        height: height,
         width: size,
         resizeMode: 'contain',
       }}
